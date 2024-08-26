7cdce2f4352edc0579495003355c839f291b3afe
TYouSK0426 <tyoushinnkou@GMAIL.COM>
2024-08-26 23:29:09 +0900

    Fix file extension case and update paths

--- scripts.js
+++ scripts.js
@@ -14,13 +14,13 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }
 
-    document.getElementById('btn1').addEventListener('click', () => playAudio('b1.Mp3'));
-    document.getElementById('btn2').addEventListener('click', () => playAudio('b2.Mp3'));
-    document.getElementById('btn3').addEventListener('click', () => playAudio('b3.Mp3'));
-    document.getElementById('btn4').addEventListener('click', () => playAudio('b4.Mp3'));
-    document.getElementById('btn5').addEventListener('click', () => playAudio('b5.Mp3'));
-    document.getElementById('btn6').addEventListener('click', () => playAudio('b6.Mp3'));
-    document.getElementById('btn7').addEventListener('click', () => playAudio('b7.Mp3'));
+    document.getElementById('btn1').addEventListener('click', () => playAudio('b1.MP3'));
+    document.getElementById('btn2').addEventListener('click', () => playAudio('b2.MP3'));
+    document.getElementById('btn3').addEventListener('click', () => playAudio('b3.MP3'));
+    document.getElementById('btn4').addEventListener('click', () => playAudio('b4.MP3'));
+    document.getElementById('btn5').addEventListener('click', () => playAudio('b5.MP3'));
+    document.getElementById('btn6').addEventListener('click', () => playAudio('b6.MP3'));
+    document.getElementById('btn7').addEventListener('click', () => playAudio('b7.MP3'));
 
 
     document.getElementById('btn8').addEventListener('click', () => {
